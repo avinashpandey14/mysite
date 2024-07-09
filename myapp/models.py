0a866cb314371de86df37a91f4f8ebe019d78063
avinashpandey14 <pandeyavinash705@gmail.com>
2024-07-09 09:11:15 +0530

    first commit

---
+++ myapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Link(models.Model):
+    def __str__(self):
+        if self.name==None:
+            return "ERROR- NAME IS NULL"
+        else:
+            return self.name
+    address=models.CharField(max_length=1000,null=True,blank=True)
+    name=models.CharField(max_length=1000,null=True,blank=True)
